d41bb1035096a4051560d812f061e49794beb88f
haushekmiva <chuminitoGordito@proton.me>
2026-06-14 17:25:43 +0300

    feat: add user entity

---
+++ src/main/java/com/haushekmiva/model/User.java
@@ -0,0 +1,27 @@
+package com.haushekmiva.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(exclude = {"passwordHash"})
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String username;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+}
