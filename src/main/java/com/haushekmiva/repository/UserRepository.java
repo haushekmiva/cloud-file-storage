497930ac1b67c9c49dc4460f1f0b910e0bfd6d93
haushekmiva <chuminitoGordito@proton.me>
2026-06-14 17:44:07 +0300

    feat: add user repository

---
+++ src/main/java/com/haushekmiva/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.haushekmiva.repository;
+
+import com.haushekmiva.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
