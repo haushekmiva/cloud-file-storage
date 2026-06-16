cd12f616700b2b7019e9590f8c2af3fb197a258a
haushekmiva <chuminitoGordito@proton.me>
2026-06-16 18:23:19 +0300

    feat: add error handler

--- src/main/java/com/haushekmiva/cloudfilestorage/exception/GlobalExceptionHandler.java
+++ src/main/java/com/haushekmiva/cloudfilestorage/exception/GlobalExceptionHandler.java
@@ -1,28 +1,37 @@
     package com.haushekmiva.cloudfilestorage.exception;
 
     import com.haushekmiva.cloudfilestorage.dto.ErrorDto;
+    import lombok.RequiredArgsConstructor;
     import lombok.extern.slf4j.Slf4j;
+    import org.springframework.context.MessageSource;
     import org.springframework.http.HttpStatus;
     import org.springframework.web.bind.annotation.ExceptionHandler;
     import org.springframework.web.bind.annotation.ResponseStatus;
     import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+    import java.util.Locale;
+
     @RestControllerAdvice
     @Slf4j
+    @RequiredArgsConstructor
     public class GlobalExceptionHandler {
+
+        private final MessageSource messageSource;
 
         @ExceptionHandler(UserAlreadyExists.class)
         @ResponseStatus(HttpStatus.CONFLICT)
-        public ErrorDto handelUserAlreadyExists(UserAlreadyExists e) {
+        public ErrorDto handleUserAlreadyExists(UserAlreadyExists e, Locale locale) {
             log.error("User already exists: username = {}.", e.getUsername());
-            return new ErrorDto("User already exists.");
+            String message = messageSource.getMessage("error.user.already-exists", null, locale);
+            return new ErrorDto(message);
         }
 
         @ExceptionHandler(Exception.class)
         @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-        public ErrorDto handleGeneral(Exception e) {
+        public ErrorDto handleGeneral(Exception e, Locale locale) {
             log.error("Unknown error occurred.", e);
-            return new ErrorDto("Internal server error occurred.");
+            String message = messageSource.getMessage("error.internal", null, locale);
+            return new ErrorDto(message);
         }
 
     }
