25fc2ba33b2f46fb7f64452df9e9a4b4d0616abf
haushekmiva <chuminitoGordito@proton.me>
2026-06-19 19:22:00 +0300

    feat: add LoginRequest and update field validation

--- src/main/java/com/haushekmiva/cloudfilestorage/dto/LoginRequest.java
+++ src/main/java/com/haushekmiva/cloudfilestorage/dto/LoginRequest.java
@@ -4,9 +4,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-public record RegisterRequest(
-        @NotBlank @Size(min = 6, max = 20) @Pattern(regexp = "^[a-zA-Z0-9]+$") String username,
-        @NotBlank @Size(min = 6, max = BCRYPT_MAX_LENGTH) @Pattern(regexp = "^[\\x20-\\x7E]+$") String password
+public record LoginRequest(
+        @NotBlank @Size(max = 20) String username,
+        @NotBlank @Size(max = BCRYPT_MAX_LENGTH) @Pattern(regexp = "^[\\x20-\\x7E]+$") String password
 ) {
     public static final int BCRYPT_MAX_LENGTH = 72;
 }
