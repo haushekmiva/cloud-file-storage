0a1854e0819a0fd915c8ab6bd93c121b45098641
haushekmiva <chuminitoGordito@proton.me>
2026-08-07 16:17:13 +0300

    feat: add uploadResource controller

--- src/main/java/com/haushekmiva/cloudfilestorage/controller/FileStorageController.java
+++ src/main/java/com/haushekmiva/cloudfilestorage/controller/FileStorageController.java
@@ -7,10 +7,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/resource")
@@ -23,9 +23,18 @@ public class FileStorageController {
     @GetMapping
     public ResponseEntity<ResourceInfoResponse> getResourceInfo(@RequestParam String path,
                                                                 @AuthenticationPrincipal UserDetailsImpl userDetails) {
-
         ResourceInfoResponse response = resourceService.getResourceInfo(path, userDetails.user().getId());
         return ResponseEntity.ok(response);
     }
+
+    @PostMapping
+    public ResponseEntity<List<ResourceInfoResponse>> uploadResource(@RequestParam String path,
+                                                                     List<MultipartFile> files,
+                                                                     @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        List<ResourceInfoResponse> responses = resourceService.upload(files, path, userDetails.user().getId());
+        return ResponseEntity.ok(responses);
+    }
+
+
 
 }
