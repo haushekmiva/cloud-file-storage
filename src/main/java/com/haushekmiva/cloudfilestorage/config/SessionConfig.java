912e7603bbba8f47d2acc4b8d3297441d23c660f
haushekmiva <chuminitoGordito@proton.me>
2026-08-15 16:44:31 +0300

    fix: add BasicPolymorphicTypeValidator to correct serialization work

--- src/main/java/com/haushekmiva/cloudfilestorage/config/SessionConfig.java
+++ src/main/java/com/haushekmiva/cloudfilestorage/config/SessionConfig.java
@@ -6,7 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.security.jackson.SecurityJacksonModules;
+import tools.jackson.databind.ObjectMapper;
 import tools.jackson.databind.json.JsonMapper;
+import tools.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import tools.jackson.databind.jsontype.PolymorphicTypeValidator;
+import tools.jackson.databind.DefaultTyping;
 
 @Configuration
 public class SessionConfig implements BeanClassLoaderAware {
@@ -15,9 +19,16 @@ public class SessionConfig implements BeanClassLoaderAware {
 
     @Bean
     public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
-        JsonMapper mapper = JsonMapper.builder()
-                .addModules(SecurityJacksonModules.getModules(loader))
+        BasicPolymorphicTypeValidator.Builder typeValidatorBuilder = BasicPolymorphicTypeValidator.builder()
+                .allowIfSubType("com.haushekmiva.cloudfilestorage.") // Разрешаем весь пакет
+                .allowIfSubType("java.util.")                     // Для коллекций (List, Map и т.д.)
+                .allowIfSubType("java.lang.");                    // Для стандартных типов (String, Long и т.д.)
+
+
+        var mapper = JsonMapper.builder()
+                .addModules(SecurityJacksonModules.getModules(loader, typeValidatorBuilder))
                 .build();
+
         return new GenericJacksonJsonRedisSerializer(mapper);
     }
 
