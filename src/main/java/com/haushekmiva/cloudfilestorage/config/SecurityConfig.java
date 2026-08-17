e8f5bb75596c6a2ee82e362c830c7e324248bea5
haushekmiva <chuminitoGordito@proton.me>
2026-08-17 20:31:33 +0300

    fix: set correct response code for logout endpoint

--- src/main/java/com/haushekmiva/cloudfilestorage/config/SecurityConfig.java
+++ src/main/java/com/haushekmiva/cloudfilestorage/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
                 .logout(logout -> logout
                         .logoutUrl("/api/auth/sign-out")
                         .logoutSuccessHandler((request, response, authentication) ->
-                                response.setStatus(HttpServletResponse.SC_OK)));
+                                response.setStatus(HttpServletResponse.SC_NO_CONTENT)));
 
         return http.build();
     }
